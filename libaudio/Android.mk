5832ca13ab5826782ee75c419b0d622e314755d0
Daveee10 <daver16@msn.com>
2012-10-22 21:45:10 +0200

    Some reorder and cleanup

--- libaudio/Android.mk
+++ libaudio/Android.mk
@@ -15,64 +15,66 @@
 
 ifeq ($(BUILD_JBMP_TEST_AUDIO),true)
 
-ifneq ($(BUILD_TINY_ANDROID),true)
-
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := audio_policy.$(TARGET_BOARD_PLATFORM)
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+    LOCAL_CFLAGS += -DQCOM_HARDWARE
+endif
+
+LOCAL_SRC_FILES := AudioHardware.cpp
+
+ifeq ($(BOARD_HAVE_BLUETOOTH),true)
+  LOCAL_CFLAGS += -DWITH_A2DP
+endif
+
+LOCAL_SHARED_LIBRARIES := \
+    libcutils       \
+    libutils        \
+    libmedia
+
+LOCAL_STATIC_LIBRARIES := \
+    libmedia_helper  \
+    libaudiohw_legacy
+
+LOCAL_MODULE := audio.primary.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_STATIC_LIBRARIES := libmedia_helper
-LOCAL_WHOLE_STATIC_LIBRARIES := libaudiopolicy_legacy
 LOCAL_MODULE_TAGS := optional
+
+LOCAL_CFLAGS += -fno-short-enums
+
+LOCAL_C_INCLUDES += hardware/libhardware/include
+LOCAL_C_INCLUDES += hardware/libhardware_legacy/include
+LOCAL_C_INCLUDES += frameworks/base/include
+LOCAL_C_INCLUDES += system/core/include
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := AudioPolicyManager.cpp
 
 LOCAL_SHARED_LIBRARIES := \
     libcutils \
     libutils \
     libmedia
 
-LOCAL_SRC_FILES:= AudioPolicyManager.cpp
-
-ifeq ($(BOARD_HAVE_BLUETOOTH),true)
-    LOCAL_CFLAGS += -DWITH_A2DP
-endif
-
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := audio.primary.$(TARGET_BOARD_PLATFORM)
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_SHARED_LIBRARIES := \
-    libcutils \
-    libutils \
-    libmedia \
-    libdl
-
 LOCAL_STATIC_LIBRARIES := \
+    libaudiopolicy_legacy \
     libmedia_helper \
     libaudiohw_legacy
 
-LOCAL_SRC_FILES := \
-    AudioHardware.cpp \
-    audio_hw_hal.cpp
-
-LOCAL_CFLAGS += -fno-short-enums
-
-LOCAL_C_INCLUDES := hardware/libhardware/include
-LOCAL_C_INCLUDES += hardware/libhardware_legacy/include
-LOCAL_C_INCLUDES += frameworks/base/include
-LOCAL_C_INCLUDES += system/core/include
+LOCAL_MODULE := audio_policy.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_TAGS := optional
 
 ifeq ($(BOARD_HAVE_BLUETOOTH),true)
-#  LOCAL_WHOLE_SHARED_LIBRARIES += audio.a2dp.default
+  LOCAL_CFLAGS += -DWITH_A2DP
 endif
+
+LOCAL_C_INCLUDES := hardware/libhardware_legacy/audio
 
 include $(BUILD_SHARED_LIBRARY)
 
-endif # not BUILD_TINY_ANDROID
-
-endif # BUILD_JBMP_TEST_AUDIO
+endif
