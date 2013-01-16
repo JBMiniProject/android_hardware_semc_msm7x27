57f6adeba76305345b79f13205ec43e5e296c2d3
Daveee10 <daver16@msn.com>
2013-01-16 20:57:53 +0100

    Libaudio: Update Android.mk

--- libaudio/Android.mk
+++ libaudio/Android.mk
@@ -1,10 +1,11 @@
-ifeq ($(BUILD_JBMP_TEST_AUDIO),true)
+$(shell mkdir -p $(OUT)/obj/STATIC_LIBRARIES/libaudiohw_legacy_intermediates)
+$(shell touch $(OUT)/obj/STATIC_LIBRARIES/libaudiohw_legacy_intermediates/export_includes)
 
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := audio_policy.msm7x27
+LOCAL_MODULE := audio_policy.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_STATIC_LIBRARIES := libmedia_helper
 LOCAL_WHOLE_STATIC_LIBRARIES := libaudiopolicy_legacy
@@ -22,9 +23,13 @@ ifeq ($(BOARD_HAVE_BLUETOOTH),true)
 endif
 
 include $(BUILD_SHARED_LIBRARY)
+
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := audio.primary.msm7x27
+$(shell mkdir -p $(OUT)/obj/STATIC_LIBRARIES/libaudiohw_legacy_intermediates)
+$(shell touch $(OUT)/obj/STATIC_LIBRARIES/libaudiohw_legacy_intermediates/export_includes)
+
+LOCAL_MODULE := audio.primary.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_STATIC_LIBRARIES += libmedia_helper
 LOCAL_WHOLE_STATIC_LIBRARIES := libaudiohw_legacy
@@ -42,5 +47,3 @@ LOCAL_SRC_FILES += AudioHardware.cpp
 LOCAL_CFLAGS += -fno-short-enums
 
 include $(BUILD_SHARED_LIBRARY)
-
-endif
