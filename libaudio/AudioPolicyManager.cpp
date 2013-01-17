ee1bdbd9618bd0048351bb9dae627a0121a3fca4
Daveee10 <daver16@msn.com>
2013-01-17 12:40:41 +0100

    Libaudio: Change Log tag

--- libaudio/AudioPolicyManager.cpp
+++ libaudio/AudioPolicyManager.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "AudioPolicyManager"
+#define LOG_TAG "AudioPolicyManagerMSM72XX"
 //#define LOG_NDEBUG 0
 #include <utils/Log.h>
 #include "AudioPolicyManager.h"
