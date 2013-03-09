4199ebb5780732801f2809246c95e41376a29a44
Daveee10 <daver16@msn.com>
2013-03-09 14:37:02 +0100

    libaudio: Higher volume

--- libaudio/AudioPolicyManager.cpp
+++ libaudio/AudioPolicyManager.cpp
@@ -232,7 +232,7 @@ status_t AudioPolicyManager::checkAndSetVolume(int stream, int index, audio_io_h
     }
 
     float volume = computeVolume(stream, index, output, device);
-    volume *= 0.15;
+    volume *= 0.30;
     // We actually change the volume if:
     // - the float value returned by computeVolume() changed
     // - the force flag is set
